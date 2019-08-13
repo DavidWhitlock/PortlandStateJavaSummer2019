49501cc025f690da2f2e57e93c2cf4e1c377ed50
David Whitlock <david.m.whitlock@gmail.com>
2019-08-12 20:17:47 -0700

    Add file with the name of a class on the forbidden file list.

---
+++ FirstAndroidApplication/app/src/main/java/edu/pdx/cs410J/whitlock/myapplication/ParserException.java
@@ -0,0 +1,7 @@
+package edu.pdx.cs410J.whitlock.myapplication;
+
+/**
+ * Class whose name is on the "forbidden list" to test out the SubmitAndroidProject program.
+ */
+public class ParserException {
+}
